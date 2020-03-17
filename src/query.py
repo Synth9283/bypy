a75339d155025d364ae95653cb23cca822362f12
SKane <le_cas315@hotmail.com>
2020-03-18 02:37:02 +1000

    v 1.0

--- src/query.py
+++ src/query.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,12 +10,27 @@ class Query:
         driver.get(link)
         wait = WebDriverWait(driver, 10)
         wait.until(lambda driver: driver.current_url != link)
-        skips = driver.find_elements_by_css_selector('#skip_bu2tton [href]')
-        link = [skip.get_attribute('href') for skip in skips]
-        print(link)
+        time.sleep(6)
+        skip = driver.find_element_by_xpath('//*[@id="skip_bu2tton"]').get_attribute('href')
+        direct_link = ''
+        for i in skip.split('.'):
+            if i == skip.split('.')[0]:
+                direct_link = 'https://www5'
+            else:
+                direct_link = f'{direct_link}.{i}'
+        driver.get(direct_link)
 
-    
     def ch(link):
-        driver = webdriver.Chrome()
+        driver = webdriver.Firefox()
         driver.get(link)
-# the linK: http://maetrimal.com/1B3q
+        wait = WebDriverWait(driver, 10)
+        wait.until(lambda driver: driver.current_url != link)
+        time.sleep(6)
+        skip = driver.find_element_by_xpath('//*[@id="skip_bu2tton"]').get_attribute('href')
+        direct_link = ''
+        for i in skip.split('.'):
+            if i == skip.split('.')[0]:
+                direct_link = 'https://www5'
+            else:
+                direct_link = f'{direct_link}.{i}'
+        driver.get(direct_link)
